3b5db95900564d47829016880dcbf6279ee441e1
LiangJiSama <mingxiaoxi22@gmail.com>
2020-02-23 17:47:06 +0800

    a school project

---
+++ cmake-build-debug/CMakeFiles/DecodeMessageFromVideo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DecodeMessageFromVideo.dir/main.cpp.o"
+  "DecodeMessageFromVideo"
+  "DecodeMessageFromVideo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DecodeMessageFromVideo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
